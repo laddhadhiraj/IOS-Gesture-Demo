bed06a64fb9d27c241923574c83cc81f58aec6d3
cmdr-surender-singh <surender@cmdr.in>
2019-12-18 15:49:54 +0530

    update Readme file

--- GestureTap/ViewController/SelectGestureController.swift
+++ GestureTap/ViewController/SelectGestureController.swift
@@ -9,22 +9,81 @@
 import UIKit
 
 class SelectGestureController: UIViewController {
-
+    @IBOutlet var leftBarBtn: UIBarButtonItem!
+    @IBOutlet var table: UITableView!
+    var tableData = ["1 Finger","2 Finger","3 Finger","4 Finger","5 Finger"]
+    var touchCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        isLeftBarButtonShow(false)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func isLeftBarButtonShow(_ show: Bool) {
+        if show {
+            navigationItem.leftBarButtonItems = [leftBarBtn]
+            title = "Select Gesture Type"
+        }else {
+            navigationItem.leftBarButtonItem = nil
+            title = "Test Gesture"
+        }
     }
-    */
+    
+    @IBAction func back(_ sender: UIBarButtonItem) {
+        isLeftBarButtonShow(false)
+        tableData = ["1 Finger","2 Finger","3 Finger","4 Finger","5 Finger"]
+        table.reloadData()
+    }
+    
+    //MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let gestureVC = segue.destination as? ViewController {
+            gestureVC.touchCounts = touchCount
+            guard let gestureType = sender as? String else { return }
+            let type = gestureType.components(separatedBy: " ").first?.lowercased() ?? "right"
+            switch type {
+            case "right":
+                gestureVC.gestureType = .right
+                gestureVC.title = "Right Gesture (\(touchCount) Finger)"
+                gestureVC.swipeRightGesture.isEnabled = true
+            case "up":
+                gestureVC.gestureType = .up
+                gestureVC.title = "Up Gesture (\(touchCount) Finger)"
+                gestureVC.swipeUpGesture.isEnabled = true
+            case "tap":
+                gestureVC.gestureType = .tap
+                gestureVC.title = "Tap Gesture (\(touchCount) Finger)"
+                gestureVC.tapGesture.isEnabled = true
+            default: break
+            }
+        }
+    }
+}
 
+extension SelectGestureController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = tableData[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let testGesture = tableData[indexPath.row] as? String, testGesture.lowercased().contains("finger") {
+            let fingerCountString = testGesture.components(separatedBy: " ").first
+            touchCount = Int(fingerCountString ?? "0") ?? 0
+            tableData = ["Right Swipe Gesture", "Up Swipe Gesture", "Tap Gesture"]
+            isLeftBarButtonShow(true)
+            table.reloadData()
+        }else {
+            performSegue(withIdentifier: "gesture", sender: tableData[indexPath.row])
+        }
+    }
 }
--- GestureTap/ViewController/ViewController.swift
+++ GestureTap/ViewController/ViewController.swift
@@ -5,35 +5,59 @@
 //  Created by Dhiraj Laddha on 09/12/19.
 //  Copyright © 2019 Surender Singh. All rights reserved.
 //
+enum Gestures {
+    case right
+    case up
+    case tap
+}
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    private var rightSwipeCount = 0
-    private var upSwipeCount = 0
-    var isTimer = true
-    @IBOutlet var rightSwipeLbl: UILabel!
-    @IBOutlet var upSwipeLbl: UILabel!
     @IBOutlet var tapLbl: UILabel!
-    @IBOutlet var userGuideImg: UIImageView!
     @IBOutlet var swipeUpGesture: UISwipeGestureRecognizer!
     @IBOutlet var tapGesture: UITapGestureRecognizer!
     @IBOutlet var swipeRightGesture: UISwipeGestureRecognizer!
+    
+    private var rightSwipeCount = 0
+    private var upSwipeCount = 0
+    var isTimer = true
+    var touchCounts = 0
+    var gestureType = Gestures.tap
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //view.isMultipleTouchEnabled = false
+        tapLabelText()
+        setGestureTouchCount(touchCounts)
     }
     
+    // tap label text setUp --
+    fileprivate func tapLabelText() {
+        if self.gestureType == .right {
+            self.tapLbl.text = "→ Right Swipe 3 Times"
+        }else if self.gestureType == .up  {
+            self.tapLbl.text = "↑ Up Swipe 4 Times Fast"
+        }
+    }
+    
+    // set gesture tap count --
+    private func setGestureTouchCount(_ count: Int) {
+        tapGesture.numberOfTouchesRequired = count
+        swipeRightGesture.numberOfTouchesRequired = count
+        swipeUpGesture.numberOfTouchesRequired = count
+    }
+    
+    // back btn tap --
+    @IBAction func back(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: false)
+    }
+
     //MARK: - Swipe Gesture -
     @IBAction func swipe(_ sender: UISwipeGestureRecognizer) {
-        if userGuideImg.isHidden == false {
-            return
-        }
-        
         if sender.direction == .right { // Right Direction --
             upSwipeCount = 0
-            rightSwipeLbl.text = "Right Swipe \(2-rightSwipeCount) Times"
+            tapLbl.text = "→ Right Swipe \(2-rightSwipeCount) Times"
             if rightSwipeCount >= 2 {
                 print("right swipe 3 times")
                 rightSwipeCount = 0
@@ -45,7 +69,7 @@ class ViewController: UIViewController {
             print("right swipe")
         }else if sender.direction == .up { // Up Direction --
             rightSwipeCount = 0
-            upSwipeLbl.text = "Up Swipe \(3-upSwipeCount) Times Fast"
+            tapLbl.text = "↑ Up Swipe \(3-upSwipeCount) Times Fast"
             if upSwipeCount >= 3 {
                 print("up swipe 4 times")
                 upSwipeCount = 0
@@ -60,66 +84,19 @@ class ViewController: UIViewController {
         if isTimer {  // reset timer --
             isTimer = false
             // start timer for 2 second
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                 self.rightSwipeCount = 0
                 self.upSwipeCount = 0
                 self.isTimer = true
-                self.rightSwipeLbl.text = "Right Swipe 3 Times"
-                self.upSwipeLbl.text = "Up Swipe 4 Times Fast"
+                self.tapLabelText()
             }
         }
     }
     //MARK: - Tap Gesture
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
-        if userGuideImg.isHidden == false {
-            return
-        }
         showAlert(title: "Congrats! You found one of our secret gestures! This was the 5-Times-Rapid-Tap gesture.")
     }
-    
-    //MARK: - Tap to start -
-    @IBAction func tapToStart(_ sender: UIButton) {
-        userGuideImg.isHidden = true
-        sender.isHidden = true
-    }
-    
-    @IBAction func fingersTap(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Select finger touch count", message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "1", style: .default, handler: { (action) in
-            print("1 Finger Touch on View")
-            self.showAlert(title: "Now you can swipe, tap with 1 finger")
-            self.setGestureTouchCount(1)
-        }))
-        alert.addAction(UIAlertAction(title: "2", style: .default, handler: { (action) in
-            print("2 Finger Touch on View")
-            self.showAlert(title: "Now you can swipe, tap with 2 fingers")
-            self.setGestureTouchCount(2)
-        }))
-        alert.addAction(UIAlertAction(title: "3", style: .default, handler: { (action) in
-            print("3 Finger Touch on View")
-            self.showAlert(title: "Now you can swipe, tap with 3 fingers")
-            self.setGestureTouchCount(3)
-        }))
-        alert.addAction(UIAlertAction(title: "4", style: .default, handler: { (action) in
-            print("4 Finger Touch on View")
-            self.showAlert(title: "Now you can swipe, tap with 4 fingers")
-            self.setGestureTouchCount(4)
-        }))
-        alert.addAction(UIAlertAction(title: "5", style: .default, handler: { (action) in
-            print("5 Finger Touch on View")
-            self.showAlert(title: "Now you can swipe, tap with 5 fingers")
-            self.setGestureTouchCount(5)
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
-        present(alert, animated: true, completion: nil)
-        
-    }
-    
-    private func setGestureTouchCount(_ count: Int) {
-        tapGesture.numberOfTouchesRequired = count
-        swipeRightGesture.numberOfTouchesRequired = count
-        swipeUpGesture.numberOfTouchesRequired = count
-    }
+
     
     //MARK: - Show AlertView --
     private func showAlert(title: String) {
@@ -128,13 +105,6 @@ class ViewController: UIViewController {
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
     }
-    
-    // Detect Touch count --
-    /*
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touchCount = event?.touches(for: view)?.count
-        print("\(touchCount ?? 0) finger touch on screen")
-        showAlert(title: "\(touchCount ?? 0) finger touch on screen")
-    }*/
+
 }
 
